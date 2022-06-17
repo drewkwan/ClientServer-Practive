87bdf5ff0e17eff79847938743303e3852feb6d2
drewkwan <andrewtimkwan@gmail.com>
2022-06-17 15:55:27 +0800

    initial commit

---
+++ src/main/java/vttp/clientserver/practice/App.java
@@ -0,0 +1,13 @@
+package vttp.clientserver.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
